c2aa4f38423379d1e7384c0c540e96b9b01e7125
Jedidiah <jedidiah.vaskis@gmail.com>
2022-12-07 10:20:17 -0700

    Replaced size changing popup with slider, and changed tracing behavior.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let length = 16; // How many cells should be on one side of the square grid.
-let container = document.getElementById("grid"); // Make grid container variable
+/* Changing grid size */
+
+let slider = document.getElementById("changeSize");
+let output = document.getElementById("size");
+output.innerHTML = slider.value;
+
+slider.addEventListener('mouseup', function changeSize() {
+    output.innerHTML = slider.value;
+    length = Number(slider.value);
+    resetGrid(length, totalCells);
+});
+
+/* First grid */
+
+let length = Number(slider.value); // How many cells should be on one side of the square grid.
+let container = document.getElementById('grid'); // Make grid container variable
 let totalCells = 0;
 
 function generateGrid (length) {
@@ -14,21 +28,43 @@ function generateGrid (length) {
         cell.style.gridColumn = presentColumn;
         cell.id = "cell #" + i;
         cell.classList.add('cell');                 // Assign the cell it's baseline class
+
         presentColumn += 1;
         if (presentColumn == (targetColumns + 1)) {
            presentRow += 1;
            presentColumn = 1;
         }
         container.appendChild(cell.cloneNode());
+
     }
-    let cells = document.getElementsByClassName('cell'); // Add traced class when hovered over
-    for (const cell of cells) {
-        cell.addEventListener('mouseover', function trace() {
-        cell.classList.add("traced");
-        });
-    };
     return totalCells;
 }
+
+generateGrid(length); // Make first grid
+
+/* Tracing behavior */
+
+function addListeners() { // Inspired by Scotty's Retro-Sketch
+    cells = document.querySelectorAll('.cell')
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('mousedown', trace);  
+        // Trace if both the mouse has entered and it is depressed.
+        cells[i].addEventListener('mouseenter', traceHover);
+    }
+}
+function trace(e) { 
+    e.target.style.backgroundColor = 'gray';
+}
+
+function traceHover(e) {
+    if (e.buttons > 0) { // If any mouse button is pressed.
+        e.target.style.backgroundColor = 'gray';
+    }
+}
+
+addListeners();
+
+/* Resetting the grid */
 
 function deleteGrid(totalCells) {
     let container = document.getElementById("grid");
@@ -41,34 +77,10 @@ function deleteGrid(totalCells) {
 function resetGrid(length, totalCells) {
     deleteGrid(totalCells);                 // Remove previous cells
     generateGrid(length);                   // Generate new grid
+    addListeners();
 }
 
-generateGrid(length); // Make first grid
-
-let cells = document.getElementsByClassName('cell');
-for (const cell of cells) {
-    cell.addEventListener('pointerenter', function trace() {
-        cell.classList.add("traced");
-    });
-};
-
-/* Changing grid size */
-
-changeButton = document.getElementById("change-button");
-changeButton.addEventListener('click', function changeSize() {
-    length = Number(prompt("Enter new square grid side length (1 to 100 accepted).", ));
-    if (length < 1 || length > 100) {
-        length = prompt("Please enter a valid size (1 to 100).", "16");
-        if (length < 1 || length > 100) {
-            changeSize();
-        } else {
-            resetGrid(length);
-        }
-    } else {    
-        resetGrid(length, totalCells);                  
-    }
-});
-
+// References
     // https://code-boxx.com/create-grid-javascript/
     // https://stackoverflow.com/questions/71607162/using-javascript-with-css-grid
     // https://stackoverflow.com/questions/51433918/filling-css-grid-with-javascript
